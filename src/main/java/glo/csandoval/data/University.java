fa599b7fa403b1a8def4d4106010809fcaf2ac6b
Carlos Sandoval <c.sandoval@globant.com>
2024-08-14 20:16:34 -0500

    feat(menu): refactor code and add methods to simplify menu

--- src/main/java/glo/csandoval/data/University.java
+++ src/main/java/glo/csandoval/data/University.java
@@ -32,6 +32,14 @@ public class University {
         University.courses.add(newCourse);
     }
 
+    public static Course addCourse(String name, String classroom) throws IllegalArgumentException {
+        if (name == null || classroom == null || name.isEmpty() || classroom.isEmpty())
+            throw new IllegalArgumentException("University: Add null class into the list");
+        Course newCourse = new Course(name, classroom);
+        University.courses.add(newCourse);
+        return newCourse;
+    }
+
     public static void removeCourse(Course courseToRemove) throws IllegalArgumentException {
         if (courseToRemove == null || !University.courses.contains(courseToRemove))
             throw new IndexOutOfBoundsException("University: The indicated course is null or not exits");
@@ -57,6 +65,13 @@ public class University {
         }
     }
 
+    public static void addTeacherToCourse(Teacher teacher, Course course) throws IllegalArgumentException, OperationNotSupportedException {
+        if (!University.courses.contains(course) || !University.teachers.contains(teacher))
+            throw new IllegalArgumentException("University: Teacher or Course no exists");
+
+        course.addTeacher(teacher);
+    }
+
     public static List<Student> getAllStudents() {
         return new ArrayList<>(University.students);
     }
@@ -67,10 +82,13 @@ public class University {
         University.students.add(newStudent);
     }
 
-    public static void addStudent(String name, int age) throws IllegalArgumentException {
+    public static Student addStudent(String name, int age) throws IllegalArgumentException {
         if (name == null || name.isEmpty() || age < 0)
             throw new IllegalArgumentException("University: Add null student into the list");
-        University.students.add(new Student(name, age));
+
+        Student newStudent = new Student(name, age);
+        University.students.add(newStudent);
+        return newStudent;
     }
 
     public static void removeStudent(Student studentToRemove) throws IllegalArgumentException, OperationNotSupportedException {
